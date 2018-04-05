193810dd0881e55f70d67e7ec1384d6a168fb117
Wlodek <w4d.dev@gmail.com>
2018-04-05 16:01:32 +0200

    receiving data in getWeather file

--- js/getLocation.jsx
+++ js/getLocation.jsx
@@ -1,61 +1,101 @@
-const dataArr = [];
+// const dataArr = [];
+//
+// $(function () {
+//
+//     let latitude = 0;
+//     let longitude = 0;
+//
+//     $(function getLocation() {
+//
+//         if(navigator.geolocation){
+//             navigator.geolocation.getCurrentPosition(getPosition);
+//         }
+//     });
+//
+//     function getPosition(position) {
+//
+//         latitude = position.coords.latitude;
+//         longitude = position.coords.longitude;
+//
+//         getAPI(latitude,longitude);
+//     }
+//
+//     function getAPI(lat,long) {
+//         $.ajax({
+//             url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&APPID=65abbbf90e06e64ea43e8b0ebaa43bb0',
+//             dataType: 'json',
+//             method: 'GET'
+//         }).done(function (res) {
+//             // console.log(res);
+//             collectWeatherData(res);
+//         }).fail(function () {
+//             console.log('something went wrong');
+//         });
+//     }
+// });
+//
+// let dataObj = '';
+//
+// function collectWeatherData(data){
+//
+//     const obj = [
+//         {
+//             name: data.name
+//         },
+//         {
+//             humidity: data.main.humidity,
+//             pressure: data.main.pressure,
+//             temp: data.main.temp
+//         },
+//         {
+//             windDirection: data.wind.deg,
+//             windSpeed: data.wind.speed
+//         }
+//     ];
+//
+// }
+//
+// // module.exports = dataObj;
+//
 
-$(function () {
+// *******************************************************************************
 
-    let latitude = 0;
-    let longitude = 0;
+//
+// let latitude = 0;
+// let longitude = 0;
+//
+// $(function getLocation() {
+//
+//     if(navigator.geolocation){
+//         navigator.geolocation.getCurrentPosition(getPosition);
+//     }
+// });
+//
+// function getPosition(position) {
+//
+//     latitude = position.coords.latitude;
+//     longitude = position.coords.longitude;
+//
+//     getAPI(latitude,longitude, (data) => {
+//         // console.log(data);
+//     })
+// }
 
-    $(function getLocation() {
 
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(getPosition);
-        }
+
+
+function getAPI(lat,long,callback) {
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&APPID=65abbbf90e06e64ea43e8b0ebaa43bb0',
+        dataType: 'json',
+        method: 'GET'
+    }).done(function (res) {
+        // console.log(res);
+        callback(res);
+    }).fail(function () {
+        console.log('something went wrong');
     });
-
-    function getPosition(position) {
-
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-
-        getAPI(latitude,longitude);
-    }
-
-    function getAPI(lat,long) {
-        $.ajax({
-            url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&APPID=65abbbf90e06e64ea43e8b0ebaa43bb0',
-            dataType: 'json',
-            method: 'GET'
-        }).done(function (res) {
-            // console.log(res);
-            collectWeatherData(res);
-        }).fail(function () {
-            console.log('something went wrong');
-        });
-    }
-});
-
-
-function collectWeatherData(data){
-
-    const dataObj = [
-        {
-            name: data.name
-        },
-        {
-            humidity: data.main.humidity,
-            pressure: data.main.pressure,
-            temp: data.main.temp
-        },
-        {
-            windDirection: data.wind.deg,
-            windSpeed: data.wind.speed
-        }
-    ];
-
-    console.log(dataObj);
-
 }
 
-
-// module.exports = dataObj;
+export {getAPI};
 
--- js/getWeather.jsx
+++ js/getWeather.jsx
@@ -1,32 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import data from './getLocation.jsx';
+import {getAPI} from './getLocation.jsx';
+
+$(function getLocation() {
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(getPosition);
+    }
+});
+
+function getPosition(position) {
+
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    getAPI(latitude,longitude, (data) => {
+
+        // tu mam dane
+        console.log(data);
+
+    })
+}
+
+// co mam zrobić żeby je mieć tutaj
+
+class Title extends React.Component {
+    render(){
+
+        return <h1>
+            {/* // a najlepiej tutaj */}
+            title
+        </h1>
+    }
+}
+
+class App extends React.Component {
+    render(){
+        return <Title />
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
-
-    class Title extends React.Component {
-        render(){
-
-            const test = data;
-
-            // console.log(test);
-
-
-            return <h1>
-                title
-            </h1>
-        }
-    }
-
-    class App extends React.Component {
-        render(){
-            return <Title />
-        }
-    }
-
     ReactDOM.render(
         <App />,
         document.getElementById('app')
     );
-
 });
