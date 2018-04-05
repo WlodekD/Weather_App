9e780cd567c3c11efc308764fbaf8984f20c29a9
Wlodek <w4d.dev@gmail.com>
2018-04-05 20:31:28 +0200

    code more DRY

--- js/getWeather.jsx
+++ js/getWeather.jsx
@@ -3,65 +3,21 @@ import ReactDOM from 'react-dom';
 import {getAPI} from './getLocation.jsx';
 
 
-$(function getLocation() {
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(getPosition);
-    }
-});
+        getAPI(position.coords.latitude, position.coords.longitude, (data) => {
 
+            // i dupa.. zagrzebane
+            console.log(data);
 
-function getPosition(position) {
-
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-
-    class GetData extends React.Component {
-        constructor(props){
-            super(props);
-            this.state = {
-                data: false,
-            };
-        }
-
-        componentDidMount(){
-            getAPI(latitude,longitude, (data) => {
-                data.load().then(this.dataLoaded);
-            })
-        }
-
-        dataLoaded = data => this.setState({data});
-
-
-        render(){
-            if(this.state.data === false){
-                return null;
-            }
-
-            const elements = this.state.data.map(el => {
-                return <div>
-                    <h1>{el.name}</h1>
-                    <div>
-                        <span>{el.main.temp}</span>
-                        <span>{el.main.pressure}</span>
-                        <span>{el.main.humidity}</span>
-                        <span>{el.main.sea_level}</span>
-                    </div>
-                </div>
-            });
-
-            return <div>
-                {elements}
-            </div>
-        }
-    }
-
+        });
+    });
 }
 
 class App extends React.Component {
     render(){
-        return <GetData />
+        return  <h1>title</h1>
     }
 }
 
@@ -71,3 +27,43 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('app')
     );
 });
+
+// class GetData extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             data: false,
+//         };
+//     }
+//
+//     componentDidMount(){
+//         getAPI(latitude,longitude, (data) => {
+//             data.load().then(this.dataLoaded);
+//         })
+//     }
+//
+//     dataLoaded = data => this.setState({data});
+//
+//
+//     render(){
+//         if(this.state.data === false){
+//             return null;
+//         }
+//
+//         const elements = this.state.data.map(el => {
+//             return <div>
+//                 <h1>{el.name}</h1>
+//                 <div>
+//                     <span>{el.main.temp}</span>
+//                     <span>{el.main.pressure}</span>
+//                     <span>{el.main.humidity}</span>
+//                     <span>{el.main.sea_level}</span>
+//                 </div>
+//             </div>
+//         });
+//
+//         return <div>
+//             {elements}
+//         </div>
+//     }
+// }
