6e0c5c80aee06255a7bf91aaa0a3ac01ccbc2e08
Wlodek <w4d.dev@gmail.com>
2018-04-10 03:02:57 +0200

    new icons added

--- js/getWeather.jsx
+++ js/getWeather.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {getAPI} from './getLocation.jsx';
+import weatherIcons from './icons.json';
 
-class LocationNameComponent extends React.Component {
+
+class LocationBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            data : false,
+        }
+    }
+
+    componentDidMount(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(position => {
+
+                getAPI(position.coords.latitude, position.coords.longitude, (data) => {
+
+                    if(data !== false){
+
+                        this.setState({
+                            data: data
+                        });
+                    }
+                });
+            });
+        }
+    };
+
+
+    render(){
+        if(this.state.data === false){
+            return null;
+        }
+
+        const locationName = this.state.data.name;
+
+        return <div className='locationBox'>
+            <p className='locationName'>Your location: <span>{locationName}</span></p>
+        </div>
+    }
+}
+
+class TemperatureBox extends React.Component{
     constructor(props){
         super(props);
         this.state = {
@@ -54,28 +95,52 @@ class LocationNameComponent extends React.Component {
 
         // in Celcius
         const tempC = this.state.data.main.temp - 273.15;
-        const tempF = (this.state.data.main.temp - 273.15) * (9/5) + 32;
-        const weatherIcon = this.state.data.weather[0].icon;
+        const tempF = ((this.state.data.main.temp - 273.15) * (9/5) + 32).toFixed(1);
 
-        return <div className='mainWrapper'>
-                <div className='locationBox'>
-                    <p className='locationName'>Your location: <span>{locationName}</span></p>
-                </div>
-                <div className='tempBox'>
-                    <div><img src={"http://openweathermap.org/img/w/"+weatherIcon+".png"} alt="weather icon"/></div>
+        const description = this.state.data.weather["0"].description;
+        const prefix = 'wi wi-';
+        const code = this.state.data.weather[0].id;
+        let icon = weatherIcons[code].icon;
+
+        if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+            icon = 'day-' + icon;
+        }
+
+        icon = prefix+icon;
+
+        return <div className='tempBox'>
+            <div>
+                <i className={icon}/>
+                <p>{description}</p>
+            </div>
+            <div>
+                <div>
                     <p style={{display: this.state.tempC}}>{tempC}</p>
                     <p style={{display: this.state.tempF}}>{tempF}</p>
+                </div>
+                <span>
                     <a onClick={this.showTempInC} href="#">&deg;C</a>
                     <a>|</a>
                     <a onClick={this.showTempInF} href="#">&deg;F</a>
-                </div>
+                </span>
             </div>
+        </div>
+    }
+}
+
+class MainWrapper extends React.Component {
+
+    render(){
+        return <div className='mainWrapper'>
+            <LocationBox />
+            <TemperatureBox />
+        </div>
     }
 }
 
 class App extends React.Component {
     render(){
-        return <LocationNameComponent />
+        return <MainWrapper />
     }
 }
 
