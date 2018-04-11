a635c69aa112323661b24cda5f0bbc4a544d37ad
Wlodek <w4d.dev@gmail.com>
2018-04-11 15:04:19 +0200

    air quality functionality added

--- js/airQualityInfo.jsx
+++ js/airQualityInfo.jsx
@@ -4,54 +4,99 @@ class AirQualityInfo extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            data : false,
             lat: 0,
-            long: 0
+            long: 0,
         }
     }
 
     componentDidMount() {
-        $.ajax({
-            url: 'https://cors-anywhere.herokuapp.com/https://api.gios.gov.pl/pjp-api/rest/station/findAll',
-            dataType: 'json',
-            method: 'GET'
-        }).done(function (res) {
-            getAirData(res)
-        }).fail(function () {
-            console.log('something went wrong');
+
+        fetch('https://cors-anywhere.herokuapp.com/https://api.gios.gov.pl/pjp-api/rest/station/findAll').then( resp => {
+            if (resp.ok)
+                return resp.json();
+            else
+                throw new Error('Something went wrong');
+        }).then( data => {
+
+            const latU = Number(this.state.lat);
+            const longU = Number(this.state.long);
+
+            let minDist = Infinity;
+            let closestID;
+            data.forEach(el => {
+               const {gegrLat, gegrLon, id} = el;
+               const dist = (Math.abs(latU - gegrLat)) + (Math.abs(longU - gegrLon));
+               if(dist < minDist){
+                   minDist = dist;
+                   closestID = id;
+               }
+            });
+
+            this.getID(closestID);
+
+        }).catch( err => {
+            console.log('error! ', err);
         });
+
+        this.getID = (id) => {
+            fetch('https://cors-anywhere.herokuapp.com/https://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/'+id)
+                .then(r => r.json())
+                .then( data => {
+                    this.setState({
+                        c6h6: data.c6h6IndexLevel.indexLevelName,
+                        co: data.coIndexLevel.indexLevelName,
+                        no2: data.no2IndexLevel.indexLevelName,
+                        pm10: data.pm10IndexLevel.indexLevelName,
+                        pm25: data.pm25IndexLevel.indexLevelName
+                    });
+                });
+        };
 
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(position => {
                 this.setState({
-                    lat: position.coords.latitude.toFixed(6),
-                    long: position.coords.longitude.toFixed(6)
-                })
+                    lat: position.coords.latitude.toFixed(4),
+                    long: position.coords.longitude.toFixed(4)
+                });
             });
         }
-
-        function getAirData(data) {
-            // console.log(data[0].gegrLat);
-            // console.log(data[0].gegrLon);
-            console.log(data);
-        }
-
     }
 
     render(){
 
-        // if(this.state.data === false){
-        //     return null;
-        // }
-
-        console.log(this.state.lat);
-        console.log(this.state.long);
-
-        return <div className='airQualityBox'>
-
-        </div>
+        if(this.state.c6h6 === false){
+            return null;
+        } else {
+            return <div className='airQualityBox'>
+                <h1>Index jakości powietrza</h1>
+                <div className='airQualityInfo1'>
+                    <div className='chh6'>
+                        <p>Wskaźnik benzenu</p>
+                        <p>{this.state.c6h6}</p>
+                    </div>
+                    <div className='co'>
+                        <p>Wskaźnik tlenku węgla</p>
+                        <p>{this.state.co}</p>
+                    </div>
+                    <div className='no2'>
+                        <p>Wskaźnik dwutlenku azotu </p>
+                        <p>{this.state.no2}</p>
+                    </div>
+                </div>
+                <hr/>
+                <div className='airQualityInfo2'>
+                    <div className='no2'>
+                        <p>Wskaźnik pyłów zawieszonych pm10 </p>
+                        <p>{this.state.pm10}</p>
+                    </div>
+                    <div className='no2'>
+                        <p>Wskaźnik pyłów zawieszonych pm2.5 </p>
+                        <p>{this.state.pm25}</p>
+                    </div>
+                </div>
+                </div>
+        }
     }
 }
-
 
 export {AirQualityInfo}
--- js/getWeather.jsx
+++ js/getWeather.jsx
@@ -10,7 +10,7 @@ class MainWrapper extends React.Component {
         return <div className='mainWrapper'>
             <LocationBox />
             <TemperatureBox />
-            {/*<AirQualityInfo />*/}
+            <AirQualityInfo />
         </div>
     }
 }
